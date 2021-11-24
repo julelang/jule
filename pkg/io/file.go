9a29c90c0c1a700b00799e9820a06b12e418bd29
mertcandav <mertcandav@outlook.com>
2021-11-24 17:00:26 +0300

    Fix io operations of file system.

--- pkg/io/file.go
+++ pkg/io/file.go
@@ -1,7 +1,18 @@
 package io
 
+import "os"
+
 // FILE is source file.
 type FILE struct {
 	Path    string
 	Content []rune
+}
+
+// WriteFileTruncate writes file truncate
+// by specified path and content.
+func WriteFileTruncate(path string, content []byte) error {
+	if _, err := os.Open(path); err == nil {
+		os.Remove(path)
+	}
+	return os.WriteFile(path, content, 0x025E)
 }
