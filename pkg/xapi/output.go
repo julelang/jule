9b486ffaa196fc58e82c5c80d19ca74c23e7d540
mertcandav <mertcandav@outlook.com>
2022-06-21 20:57:18 +0300

    Remove unnecessary includes from cxx output

--- pkg/xapi/output.go
+++ pkg/xapi/output.go
@@ -22,13 +22,11 @@ var CxxDefault = `#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) ||
 #include <iostream>
 #include <string>
 #include <sstream>
-#include <string.h>
 #include <functional>
 #include <vector>
 #include <map>
 #include <thread>
 #ifdef _WINDOWS
-#include <locale>
 #include <codecvt>
 #include <windows.h>
 #include <fcntl.h>
