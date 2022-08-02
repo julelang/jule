d3b8fea1700d1afa1e0f639a4781bf6cecb0b0b2
Mertcan DAVULCU <mertcandav@outlook.com>
2022-08-02 12:24:00 +0300

    Fix: std::unsafe

---
+++ std/unsafe/unsafe.hpp
@@ -0,0 +1,18 @@
+// Copyright 2022 The X Programming Language.
+// Use of this source code is governed by a BSD 3-Clause
+// license that can be found in the LICENSE file.
+
+// Depends:
+//   - api/xxc.hpp
+
+#ifndef __XXC_STD_UNSAFE_UNSAFE_HPP
+#define __XXC_STD_UNSAFE_UNSAFE_HPP
+
+template<typename T>
+inline ptr<T> __xxc_uintptr_cast_to_raw(const uintptr_xt &_Addr) noexcept;
+
+template<typename T>
+inline ptr<T> __xxc_uintptr_cast_to_raw(const uintptr_xt &_Addr) noexcept
+{ return (T*)(_Addr); }
+
+#endif // #ifndef __XXC_STD_UNSAFE_UNSAFE_HPP
