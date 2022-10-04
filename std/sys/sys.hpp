8c0f4aa78742b6bb2b8020ddb096b6045d063e35
mertcandav <mertcandav@outlook.com>
2022-10-04 15:28:41 +0300

    std::sys: add stat function

---
+++ std/sys/sys.hpp
@@ -0,0 +1,16 @@
+// Copyright 2022 The Jule Programming Language.
+// Use of this source code is governed by a BSD 3-Clause
+// license that can be found in the LICENSE file.
+
+// Depends:
+//   - api/julec.hpp
+
+#ifndef __JULEC_STD_SYS_SYS_HPP
+#define __JULEC_STD_SYS_SYS_HPP
+
+int_julet __julec_stat(const char *_Path, struct stat *_Stat) noexcept;
+
+int_julet __julec_stat(const char *_Path, struct stat *_Stat) noexcept
+{ return stat(_Path, _Stat); }
+
+#endif // #ifndef __JULEC_STD_SYS_SYS_HPP
