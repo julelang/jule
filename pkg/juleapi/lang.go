bb63f6479e14684d0809775c82eb714aa10a231f
mertcandav <mertcandav@outlook.com>
2022-10-17 17:16:36 +0300

    compiler: remove unnecessary ToDeferredCall function

--- pkg/juleapi/lang.go
+++ pkg/juleapi/lang.go
@@ -11,15 +11,6 @@ const CPP_IGNORE = "std::ignore"
 // SELF is the self keyword equavalent of cpp.
 const SELF = "this"
 
-// ToDeferredCall returns cpp of deferred function call expression string.
-func ToDeferredCall(expr string) string {
-	var cpp strings.Builder
-	cpp.WriteString("__JULEC_DEFER(")
-	cpp.WriteString(expr)
-	cpp.WriteString(");")
-	return cpp.String()
-}
-
 // ToConcurrentCall returns cpp of concurrent function call expression string.
 func ToConcurrentCall(expr string) string {
 	var cpp strings.Builder
