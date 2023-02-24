eabfdcb983893b4892e1234be1657c5555c7557d
mertcandav <mertcandav@outlook.com>
2023-02-24 12:44:15 +0300

    std::sys: update abstraction level

--- std/sys/README.md
+++ std/sys/README.md
@@ -3,3 +3,12 @@
 This package content varies system to system.
 Not complete portable.
 Reason of that, this package provides system dependent types, functions and etc.
+
+This package can be really unsafe.
+Many defines are wrapper for C defines.
+Therefore, they are marked as unsafe.
+Pointers are common in this package.
+
+If you don't have any idea C wrapper function what to do, please read documentation about related C define.
+
+The Windows defines includes many `windows.h` defines.
--- std/sys/syscall_unix.hpp
+++ std/sys/syscall_unix.hpp
@@ -13,11 +13,15 @@
 
 // Declarations
 
+str_jt __julec_str_from_byte_ptr(const JULEC_ID(byte) *_Ptr) noexcept;
 int_jt __julec_stat(const char *_Path, struct stat *_Stat) noexcept;
 
 // Definitions
 
+str_jt __julec_str_from_byte_ptr(const JULEC_ID(byte) *_Ptr) noexcept
+{ return ( str_jt( _Ptr ) ); }
+
 int_jt __julec_stat(const char *_Path, struct stat *_Stat) noexcept
-{ return stat(_Path, _Stat); }
+{ return ( stat( _Path, _Stat ) ); }
 
 #endif // #ifndef __JULEC_STD_SYS_SYSCALL_UNIX_HPP
