9fe92da991fb2962341b2c0a3c2e3360399da996
mertcandav <mertcandav@outlook.com>
2023-03-11 23:23:42 +0300

    sema: add lookup methods to symbol table

--- src/sema/table.go
+++ src/sema/table.go
@@ -33,3 +33,69 @@ func (st *SymbolTable) Find_package_by_path(path string) *Package {
 	}
 	return nil
 }
+
+// Returns variable by identifier and cpp linked state.
+// Returns nil if not exist any variable in this identifier.
+func (st *SymbolTable) Find_var(ident string, cpp_linked bool) *Var {
+	for _, v := range st.Vars {
+		if v.Ident == ident && v.Cpp_linked == cpp_linked {
+			return v
+		}
+	}
+	return nil
+}
+
+// Returns type alias by identifier and cpp linked state.
+// Returns nil if not exist any type alias in this identifier.
+func (st *SymbolTable) Find_type_alias(ident string, cpp_linked bool) *TypeAlias {
+	for _, ta := range st.Type_aliases {
+		if ta.Ident == ident && ta.Cpp_linked == cpp_linked {
+			return ta
+		}
+	}
+	return nil
+}
+
+// Returns struct by identifier and cpp linked state.
+// Returns nil if not exist any struct in this identifier.
+func (st *SymbolTable) Find_struct(ident string, cpp_linked bool) *Struct {
+	for _, s := range st.Structs {
+		if s.Ident == ident && s.Cpp_linked == cpp_linked {
+			return s
+		}
+	}
+	return nil
+}
+
+// Returns function by identifier and cpp linked state.
+// Returns nil if not exist any function in this identifier.
+func (st *SymbolTable) Find_fn(ident string, cpp_linked bool) *Fn {
+	for _, f := range st.Funcs {
+		if f.Ident == ident && f.Cpp_linked == cpp_linked {
+			return f
+		}
+	}
+	return nil
+}
+
+// Returns trait by identifier.
+// Returns nil if not exist any trait in this identifier.
+func (st *SymbolTable) Find_trait(ident string) *Trait {
+	for _, t := range st.Traits {
+		if t.Ident == ident {
+			return t
+		}
+	}
+	return nil
+}
+
+// Returns enum by identifier.
+// Returns nil if not exist any enum in this identifier.
+func (st *SymbolTable) Find_enums(ident string) *Enum {
+	for _, e := range st.Enums {
+		if e.Ident == ident {
+			return e
+		}
+	}
+	return nil
+}
