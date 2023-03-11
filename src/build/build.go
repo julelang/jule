498c666284506bc96b14fe5a1f43dc4a6152644d
mertcandav <mertcandav@outlook.com>
2023-03-11 23:32:49 +0300

    add license prefix to all files

--- src/build/build.go
+++ src/build/build.go
@@ -1,3 +1,7 @@
+// Copyright 2023 The Jule Programming Language.
+// Use of this source code is governed by a BSD 3-Clause
+// license that can be found in the LICENSE file.
+
 package build
 
 import (
--- src/lex/token.go
+++ src/lex/token.go
@@ -1,3 +1,7 @@
+// Copyright 2021 The Jule Programming Language.
+// Use of this source code is governed by a BSD 3-Clause
+// license that can be found in the LICENSE file.
+
 package lex
 
 import (
