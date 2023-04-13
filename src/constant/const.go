eff051d912096341b14a9df58fb0675ea12d99a8
mertcandav <mertcandav@outlook.com>
2023-04-13 19:45:46 +0300

    constant: add type methods for Const struct

--- src/constant/const.go
+++ src/constant/const.go
@@ -18,8 +18,82 @@ func New_bool(x bool) *Const { return &Const{data: x} }
 // Returns new constant value instance from string.
 func New_str(x string) *Const { return &Const{data: x} }
 
+// Returns new constant value instance from 64-bit floating-point.
+func New_f64(x float64) *Const { return &Const{data: x} }
+
 // Returns new constant value instance with nil.
 func New_nil() *Const { return &Const{data: nil} }
 
-// Returns new constant value instance from 64-bit floating point.
-func New_f64(x float64) *Const { return &Const{data: x} }
+
+//
+// Types
+//
+
+// Reports whether data is 64-bit signed integer.
+func (c *Const) Is_i64() bool {
+	switch c.data.(type) {
+	case int64:
+		return true
+
+	default:
+		return false
+	}
+}
+
+// Reports whether data is 64-bit unsigned integer.
+func (c *Const) Is_u64() bool {
+	switch c.data.(type) {
+	case uint64:
+		return true
+
+	default:
+		return false
+	}
+}
+
+// Reports whether data is boolean.
+func (c *Const) Is_bool() bool {
+	switch c.data.(type) {
+	case bool:
+		return true
+
+	default:
+		return false
+	}
+}
+
+// Reports whether data is string.
+func (c *Const) Is_str() bool {
+	switch c.data.(type) {
+	case string:
+		return true
+
+	default:
+		return false
+	}
+}
+
+// Reports whether data is 64-bit floating-point.
+func (c *Const) Is_f64() bool {
+	switch c.data.(type) {
+	case float64:
+		return true
+
+	default:
+		return false
+	}
+}
+
+// Reports whether data is nil.
+func (c *Const) Is_nil() bool { return c.data == nil }
+
+// Reports whether c and x has same type.
+func (c *Const) Are_same_types(x Const) bool {
+	switch {
+	case c.Is_i64() == x.Is_i64():
+		return true
+
+	default:
+		return false
+	}
+}
