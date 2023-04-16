5eb1e6c76f0593e079ac4920f60a1fc1dfaea6d0
mertcandav <mertcandav@outlook.com>
2023-04-16 17:25:17 +0300

    lit: add Rtoa and To_rune functions

--- src/constant/lit/bytes.go
+++ src/constant/lit/bytes.go
@@ -1,5 +1,10 @@
 package lit
 
+import (
+	"strconv"
+	"unicode/utf8"
+)
+
 // Reports whether kind is byte literal and returns
 // literal without quotes.
 //
@@ -28,4 +33,101 @@ func Is_byte_lit(kind string) (string, bool) {
 	}
 
 	return kind, is_byte
+}
+
+// Returns rune value string from bytes.
+// Bytes are represents rune literal, allows escape sequences.
+// Returns empty string if len(bytes) == 0
+func To_rune(bytes []byte) string {
+	if len(bytes) == 0 {
+		return ""
+	}
+
+	var r rune = 0
+	if bytes[0] == '\\' && len(bytes) > 1 {
+		i := 0
+		r = rune_from_esq_seq(bytes, &i)
+	} else {
+		r, _ = utf8.DecodeRune(bytes)
+	}
+
+	return Rtoa(r)
+}
+
+// Returns rune as rune value string.
+func Rtoa(r rune) string { return "0x" + strconv.FormatInt(int64(r), 16) }
+
+func try_btoa_common_esq(bytes []byte) (seq byte, ok bool) {
+	if len(bytes) < 2 || bytes[0] != '\\' {
+		return
+	}
+
+	switch bytes[1] {
+	case '\\':
+		seq = '\\'
+
+	case '\'':
+		seq = '\''
+
+	case '"':
+		seq = '"'
+
+	case 'a':
+		seq = '\a'
+
+	case 'b':
+		seq = '\b'
+
+	case 'f':
+		seq = '\f'
+
+	case 'n':
+		seq = '\n'
+
+	case 'r':
+		seq = '\r'
+
+	case 't':
+		seq = '\t'
+
+	case 'v':
+		seq = '\v'
+	}
+
+	ok = seq != 0
+	return
+}
+
+func rune_from_esq_seq(bytes []byte, i *int) rune {
+	b, ok := try_btoa_common_esq(bytes[*i:])
+	*i++
+	if ok {
+		return rune(b)
+	}
+
+	switch bytes[*i] {
+	case 'u':
+		rc, _ := strconv.ParseUint(string(bytes[*i+1:*i+5]), 16, 32)
+		*i += 4
+		r := rune(rc)
+		return r
+
+	case 'U':
+		rc, _ := strconv.ParseUint(string(bytes[*i+1:*i+9]), 16, 32)
+		*i += 8
+		r := rune(rc)
+		return r
+
+	case 'x':
+		seq := bytes[*i : *i+3]
+		*i += 2
+		b, _ := strconv.ParseUint(string(seq), 16, 8)
+		return rune(b)
+
+	default:
+		seq := bytes[*i : *i+3]
+		*i += 2
+		b, _ := strconv.ParseUint(string(seq), 8, 8)
+		return rune(b)
+	}
 }
