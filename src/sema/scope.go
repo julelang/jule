d996ceb50b48215bd95ffa66496569e3163715ce
mertcandav <mertcandav@outlook.com>
2023-05-17 13:42:36 +0300

    sema: improve ignore identifier processing for assign checking

--- src/sema/scope.go
+++ src/sema/scope.go
@@ -683,6 +683,10 @@ func (sc *_ScopeChecker) check_postfix(a *ast.AssignSt) {
 }
 
 func (sc *_ScopeChecker) is_new_assign_ident(ident string) bool {
+	if lex.Is_ignore_ident(ident) {
+		return false
+	}
+
 	return sc.table.def_by_ident(ident, false) == nil
 }
 
