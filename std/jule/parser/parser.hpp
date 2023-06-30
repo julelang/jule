faa63a4673a1c743a8a53d8df0683dedd939cd78
mertcandav <mertcandav@outlook.com>
2023-06-30 22:02:04 +0300

    minor fixes

--- std/jule/parser/parser.hpp
+++ std/jule/parser/parser.hpp
@@ -1,16 +1,16 @@
 #ifndef __JULE_STD_JULE_PARSER
 #define __JULE_STD_JULE_PARSER
 
-#include "../../api/jule.hpp"
+#include "../../../api/jule.hpp"
 
 template<typename Vector, typename Item>
-jule::Slice<Item> __jule_parser_vector_as_slice(const Vector &vec) noexcept;
+jule::Slice<Item> __jule_parser_vector_as_slice(Vector vec) noexcept;
 
 template<typename Vector, typename Item>
-jule::Slice<Item> __jule_parser_vector_as_slice(const Vector &vec) noexcept {
+jule::Slice<Item> __jule_parser_vector_as_slice(Vector vec) noexcept {
 	jule::Slice<Item> slice;
 	slice._len = vec._method_len();
-	slice.data.alloc = vec._field_heap;
+	slice.data.alloc = reinterpret_cast<Item*>(vec._field__heap);
 	return slice;
 }
 
