c3236b21d7e20d158cfa41ef09681a318efbcb1b
mertcandav <mertcandav@outlook.com>
2023-06-30 22:31:09 +0300

    std::vector: fix heap allocation safety

--- std/vector/alloc.hpp
+++ std/vector/alloc.hpp
@@ -1,7 +1,15 @@
+// Copyright 2023 The Jule Programming Language.
+// Use of this source code is governed by a BSD 3-Clause
+// license that can be found in the LICENSE file.
+
 #ifndef __JULE_STD_VECTOR
 #define __JULE_STD_VECTOR
 
 #include "../../api/jule.hpp"
+
+void **__jule_std_vector_new_heap(void) noexcept;
+
+void __jule_std_vector_delete_heap(void **heap) noexcept;
 
 template<typename Item>
 Item *__jule_std_vector_alloc(const jule::Int &n) noexcept;
@@ -14,6 +22,12 @@ Item __jule_std_vector_deref(void *heap, const jule::Int &i) noexcept;
 
 template<typename Item>
 void __jule_std_vector_heap_assign(void *heap, const jule::Int &i, const Item &item) noexcept;
+
+void **__jule_std_vector_new_heap(void) noexcept
+{ return new(std::nothrow) void*{nullptr}; };
+
+void __jule_std_vector_delete_heap(void **heap) noexcept
+{ delete heap; }
 
 template<typename Item>
 Item *__jule_std_vector_alloc(const jule::Int &n) noexcept
