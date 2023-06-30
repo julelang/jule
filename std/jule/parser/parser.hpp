f2449e34ac5b429655d6038d07e82a4cb0b10ad6
mertcandav <mertcandav@outlook.com>
2023-07-01 00:42:49 +0300

    std::jule::parser: fix Vector[T] to slice conversion

--- std/jule/parser/parser.hpp
+++ std/jule/parser/parser.hpp
@@ -9,8 +9,13 @@ jule::Slice<Item> __jule_parser_vector_as_slice(Vector vec) noexcept;
 template<typename Vector, typename Item>
 jule::Slice<Item> __jule_parser_vector_as_slice(Vector vec) noexcept {
 	jule::Slice<Item> slice;
+	if (vec._method_len() == 0)
+		return slice;
+
 	slice._len = vec._method_len();
-	slice.data.alloc = reinterpret_cast<Item*>(vec._field__heap);
+	slice._cap = vec._method_cap();
+	slice.data.alloc = *reinterpret_cast<Item**>(vec._field__heap);
+	slice._slice = &slice.data.alloc[0];
 	return slice;
 }
 
