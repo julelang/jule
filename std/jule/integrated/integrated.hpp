1a7f149da5a7f847b4609cc821b0c45e74030c8e
mertcandav <mertcandav@outlook.com>
2023-10-07 16:49:35 +0300

    std::jule::integrated: use new rules

--- std/jule/integrated/integrated.hpp
+++ std/jule/integrated/integrated.hpp
@@ -7,6 +7,9 @@
 
 #include "../../../api/jule.hpp"
 
+#define __jule_delete(heap) delete heap
+#define __jule_delete_array(heap) delete[] heap
+
 // Declarations
 
 typedef signed char __jule_signed_char;
@@ -18,40 +21,19 @@ typedef unsigned long long __jule_unsigned_long_long;
 typedef long double __jule_long_double;
 typedef bool __jule_bool;
 
-template<typename T> inline jule::Uintptr __jule_new(void);
-template<typename T> inline jule::Uintptr __jule_new_array(const jule::Int &size);
-template<typename T> inline void __jule_delete(const jule::Uintptr &heap);
-template<typename T> inline void __jule_delete_array(const jule::Uintptr &heap);
-template<typename T> inline T __jule_deref_ptr(const jule::Uintptr &heap);
-template<typename T> inline void __jule_assign_ptr(const jule::Uintptr &heap, T val);
+template<typename T> inline T *__jule_new(void);
+template<typename T> inline T *__jule_new_array(const jule::Int &size);
+
 inline jule::Str __jule_str_from_byte_ptr(const char *ptr);
 inline jule::Str __jule_str_from_byte_ptr(const jule::Byte *ptr);
 inline jule::Str __jule_type_id_of_any(const jule::Any &any);
 
 // Definitions
-template<typename T>
-inline jule::Uintptr __jule_new(void)
-{ return (jule::Uintptr)new T; }
+template<typename T> inline T *__jule_new(void)
+{ return new (std::nothrow) T; }
 
-template<typename T>
-inline jule::Uintptr __jule_new_array(const jule::Int &size)
-{ return (jule::Uintptr)new T[size]; }
-
-template<typename T>
-inline void __jule_delete(const jule::Uintptr &heap)
-{ delete (T*)heap; }
-
-template<typename T>
-inline void __jule_delete_array(const jule::Uintptr &heap)
-{ delete[] (T*)heap; }
-
-template<typename T>
-inline T __jule_deref_ptr(const jule::Uintptr &heap)
-{ return *(T*)heap; }
-
-template<typename T>
-inline void __jule_assign_ptr(const jule::Uintptr &heap, T val)
-{ *(T*)heap = val; }
+template<typename T> inline T *__jule_new_array(const jule::Int &size)
+{ return new(std::nothrow) T[size]; }
 
 inline jule::Str __jule_str_from_byte_ptr(const char *ptr)
 { return __jule_str_from_byte_ptr((const jule::Byte*)(ptr)); }
