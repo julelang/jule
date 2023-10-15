85e426553a63d4273428a033460f9adf2d64fd66
mertcandav <mertcandav@outlook.com>
2023-10-15 15:25:59 +0300

    jule: remove built-in Error trait

--- api/terminate.hpp
+++ api/terminate.hpp
@@ -15,43 +15,13 @@
 
 namespace jule {
 
-    // Error mask for terminations.
-    // It's also built-in Error trait.
-    struct Error {
-        inline virtual jule::Str _method_err(void) { return jule::Str(); };
-
-        virtual ~Error(void) {}
-
-        jule::Bool operator==(const Error&) { return false; }
-        jule::Bool operator!=(const Error &src) { return !this->operator==(src); }
-
-        friend std::ostream &operator<<(std::ostream &stream, Error error)
-        { return stream << error._method_err(); }
-    };
-
     // JuleC terminate handler.
-    void terminate_handler(void);
-
-    jule::Trait<Error> exception_to_error(const jule::Exception &exception);
-
     void terminate_handler(void) {
         try { std::rethrow_exception(std::current_exception()); }
         catch (const jule::Exception &e) {
             jule::outln(std::string("panic: ") + std::string(e.what()));
             std::exit(jule::EXIT_PANIC);
         }
-    }
-
-    jule::Trait<Error> exception_to_error(const jule::Exception &exception) {
-        struct PanicError: public Error {
-            jule::Str message;
-
-            jule::Str error(void)
-            { return this->message; }
-        };
-        struct PanicError error;
-        error.message = jule::to_str(exception.what());
-        return jule::Trait<Error>(error);
     }
 
 } // namespace jule
