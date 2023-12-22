8eb4b818af5a3c6e69dd16666744b3c9b71310fb
mertcandav <mertcandav@outlook.com>
2023-12-22 14:36:36 +0300

    std::vec: minor improvements

---
+++ std/vec/vec.hpp
@@ -0,0 +1,40 @@
+// Copyright 2023 The Jule Programming Language.
+// Use of this source code is governed by a BSD 3-Clause
+// license that can be found in the LICENSE file.
+
+#ifndef __JULE_STD_VEC
+#define __JULE_STD_VEC
+
+#include <new>
+
+#include "../../api/types.hpp"
+#include "../../api/slice.hpp"
+
+template <typename Item>
+struct StdJuleVecBuffer
+{
+    Item *heap = nullptr;
+    jule::Int len = 0;
+    jule::Int cap = 0;
+
+    StdJuleVecBuffer<Item>(void) = default;
+
+    StdJuleVecBuffer<Item>(const StdJuleVecBuffer<Item> &ref)
+    {
+        this->operator=(ref);
+    }
+
+    void operator=(const StdJuleVecBuffer<Item> &ref)
+    {
+        // Assignment to itself.
+        if (this->heap != nullptr && this->heap == ref.heap)
+            return;
+
+        this->heap = new (std::nothrow) Item[ref.len];
+        this->len = ref.len;
+        this->cap = this->len;
+        std::copy(ref.heap, ref.heap + this->len, this->heap);
+    }
+};
+
+#endif // __JULE_STD_VEC
