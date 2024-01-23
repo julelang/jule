379ee2d6f757692ec37e764ddfa521fe475eb366
mertcandav <mertcandav@outlook.com>
2024-01-23 21:12:42 +0300

    std::jule::integrated: use emit function as much as possible instead of C++ linking

--- std/jule/integrated/integrated.hpp
+++ std/jule/integrated/integrated.hpp
@@ -5,13 +5,6 @@
 #ifndef __JULE_STD_JULE_INTEGRATED_HPP
 #define __JULE_STD_JULE_INTEGRATED_HPP
 
-#include "../../../api/jule.hpp"
-
-#define __jule_delete(heap) delete heap
-#define __jule_delete_array(heap) delete[] heap
-
-// Declarations
-
 typedef signed char __jule_signed_char;
 typedef unsigned char __jule_unsigned_char;
 typedef unsigned short __jule_unsigned_short;
@@ -20,24 +13,5 @@ typedef long long __jule_long_long;
 typedef unsigned long long __jule_unsigned_long_long;
 typedef long double __jule_long_double;
 typedef bool __jule_bool;
-
-template<typename T> inline T *__jule_new(void);
-template<typename T> inline T *__jule_new_array(const jule::Int &size);
-
-inline jule::Str __jule_str_from_byte_ptr(const jule::Byte *ptr);
-inline jule::Str __jule_type_id_of_any(const jule::Any &any);
-
-// Definitions
-template<typename T> inline T *__jule_new(void)
-{ return new (std::nothrow) T; }
-
-template<typename T> inline T *__jule_new_array(const jule::Int &size)
-{ return new(std::nothrow) T[size]; }
-
-inline jule::Str __jule_str_from_byte_ptr(const jule::Byte *ptr)
-{ return jule::Str(ptr); }
-
-inline jule::Str __jule_type_id_of_any(const jule::Any &any)
-{ return jule::Str(any.type->type_id()); }
 
 #endif // ifndef __JULE_STD_JULE_INTEGRATED_HPP
