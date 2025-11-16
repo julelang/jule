fefe622978b0c1d9526cebe9bbd9d3c785725eb0
mertcandav <mertcandav@outlook.com>
2025-11-16 09:55:14 +0300

    jule: update LICENSE and copyright

--- src/julec/build.sh
+++ src/julec/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2023 The Jule Authors. All rights reserved.
+# Copyright 2023 The Jule Project Contributors. All rights reserved.
 # Use of this source code is governed by a BSD 3-Clause
 # license that can be found in the LICENSE file.
 
